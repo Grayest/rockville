24c6b1379a0071d53f3dd524004559287781c5a4
Grayest <saosurya@gmail.com>
2021-02-13 14:47:32 +0800

    Create Sygnify.js

---
+++ Sygnify.js
@@ -0,0 +1,7 @@
+<script
+    async
+    type="text/javascript"
+    id="sig-bc"
+    data-store-id="stores/reotp"
+    src="https://cdn-scripts.signifyd.com/bigcommerce/script-tag.js">   
+    </script>
